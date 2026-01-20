8fdfb36a6277fe9be36add8c2fcd8d8d7b12ce12
antonio welton <antonioweltondev@gmail.com>
2026-01-20 17:30:03 -0300

    Criei a interface e a função Random

---
+++ src/scripts/engine.js
@@ -0,0 +1,30 @@
+const emojis = [
+     "🦂",
+     "🦂",
+     "🪲",
+     "🪲",
+     "🪰",
+     "🪰",
+     "🐝",
+     "🐝",
+     "🪳",
+     "🪳",
+     "🦋",
+     "🦋",
+     "🐛",
+     "🐛",
+     "🕷️",
+     "🕷️"
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1)); 
+
+for(let i = 0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
+    
+
+}
